278365472dd1d99fa8136db9f26343a85aa33ad4
Mahima Manik <30531520+mahima-manik@users.noreply.github.com>
2023-11-29 00:02:47 +0530

    Add PR parser code (#2)

--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
+
+const { parsePR } = require('./parse')
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-async function run() {
+export async function run() {
   try {
     const files_to_ignore = core.getInput('files-to-ignore')
+
+    const pr_diff = await parsePR(
+      github.context.payload.pull_request,
+      files_to_ignore
+    )
+    console.log('PR diff is: ', pr_diff)
     core.setOutput('comments', files_to_ignore)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
-}
-
-module.exports = {
-  run
 }
---
+++ src/parse.js
@@ -0,0 +1,86 @@
+import { Octokit as OctokitRest } from '@octokit/rest'
+const core = require('@actions/core')
+
+const octokit = new OctokitRest({
+  auth: core.getInput('github-token')
+})
+
+async function getDiffString(owner, repo, pull_number) {
+  console.log('Getting diff for: ', owner, repo, pull_number)
+
+  const response = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number,
+    mediaType: {
+      format: 'diff'
+    }
+  })
+  return response.data
+}
+
+function parseDiff(diffString, files_to_ignore) {
+  const fileDiffRegex = /^diff --git a\/(.+?) b\/\1\nindex/gm
+  let match
+  const changes = []
+
+  while ((match = fileDiffRegex.exec(diffString)) !== null) {
+    const filename = match[1] // Extract filename directly from the regex match
+
+    // Ignore files in the ignore list
+    if (files_to_ignore.includes(filename)) {
+      console.log(`Ignoring ${filename}`)
+      continue
+    }
+
+    const start = match.index
+    const end = diffString.indexOf('diff --git', start + 1)
+    const fileDiff = diffString.substring(start, end > -1 ? end : undefined)
+
+    const lines = fileDiff.split('\n')
+    let codeBeforeChange = ''
+    let codeAfterChange = ''
+    let inChangeBlock = false
+
+    for (const line of lines) {
+      if (
+        line.startsWith('--- a/') ||
+        line.startsWith('+++ b/') ||
+        line.startsWith('@@')
+      ) {
+        inChangeBlock = true
+      } else if (inChangeBlock) {
+        if (line.startsWith('-')) {
+          codeBeforeChange += `${line.slice(1)}\n`
+        } else if (line.startsWith('+')) {
+          codeAfterChange += `${line.slice(1)}\n`
+        } else {
+          codeBeforeChange += `${line}\n`
+          codeAfterChange += `${line}\n`
+        }
+      }
+    }
+
+    changes.push({
+      filename,
+      code_before_change: codeBeforeChange,
+      code_after_change: codeAfterChange
+    })
+  }
+
+  return changes
+}
+
+export async function parsePR(pullRequest, files_to_ignore) {
+  const owner = pullRequest.base.repo.owner.login
+  const repo = pullRequest.base.repo.name
+  const pull_number = pullRequest.number
+  const diffString = await getDiffString(owner, repo, pull_number)
+  const changes = parseDiff(diffString, files_to_ignore)
+  console.log('Changes are: ', changes)
+  return {
+    title: pullRequest.title,
+    body: pullRequest.body,
+    changes
+  }
+}
