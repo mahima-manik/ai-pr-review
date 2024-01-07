df58a137182a5f7a073d60c0337876b1f583f692
Mahima Manik <mahimamanik.22@gmail.com>
2024-01-07 15:01:04 +0800

    Print openAI response

--- src/reviewer.js
+++ src/reviewer.js
@@ -57,13 +57,14 @@ export async function generateComments(code_changes, file_paths_to_ignore) {
 
   const system_prompt =
     'Act as a developer and review PR changes. Code changes is given as list of dictionary. ' +
-    'Each dictionary filename, code snippet before and after change. ' +
+    'Each dictionary has filename, code snippet before and after change. ' +
     'Some unchanged common lines are present in both before/after change. ' +
     'Review the changes for improvements, correctness, design, clean code, security, performance and other best practices.' +
+    'Only provide the comments that you are confident about. ' +
     'Extra files are provided for reference, but not to review. ' +
-    'Return ONLY list of dictionary in format: ' +
+    'Return ONLY a list of dictionary in format: ' +
     '[{"path": "path/to/file", "position": line_number on code_after_change, "body": "comment"}].' +
-    'If you have no comments, return an empty list. '
+    'If you have no comments, return an empty list.'
 
   const user_prompt = `Files to review: 
     ${JSON.stringify(code_changes)} \n
@@ -80,7 +81,7 @@ export async function generateComments(code_changes, file_paths_to_ignore) {
     ]
   })
 
-  console.log(response)
+  console.log('Message from OpenAI is', response.choices[0].message.content)
   const comments_list = JSON.parse(response.choices[0].message.content)
   return comments_list
 }
