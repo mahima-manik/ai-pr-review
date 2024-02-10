340e1107aff7089d743bd122e3f135af90e620ca
Mahima Manik <mahimamanik.22@gmail.com>
2024-02-11 01:04:31 +0530

    Add llm interface

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 const { PullRequest } = require('./pull_request.js')
 const { AIReviewer } = require('./ai_reviewer.js')
+const { OpenAIInterface } = require('./llm_interface.js')
 
 const core = require('@actions/core')
 const github = require('@actions/github')
@@ -16,8 +17,15 @@ export async function run() {
     console.log('Pull request is: ', pull_request.pr_branch_name)
     const reviewer = new AIReviewer(pull_request)
     await reviewer.formatPrChanges()
-
     console.log('Response is: ', reviewer.fomatted_changes)
+
+    const openai_key = core.getInput('openai-key')
+    const openai_interface = new OpenAIInterface(openai_key)
+    const comments_list = openai_interface.getCommentsonPR(
+      reviewer.fomatted_changes
+    )
+    console.log('Comments are: ', comments_list)
+    pull_request.addReview(comments_list)
   } catch (error) {
     // Fail the workflow run if an error occurs
     console.error(error)
